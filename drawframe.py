371e5fe99c21aa633e0f8fc65421c23a58dcc549
Matt Harriger <mharriger@caredx.com>
2022-07-11 09:43:11 -0500

    Add circle centers and a grid

--- drawframe.py
+++ drawframe.py
@@ -7,15 +7,33 @@ def bisectSegments(left, center, right):
     vRight = right - center
     return (vRight.normalized() + vLeft.normalized()).normalized() * .9525
 
+def drawCenterMark(context, point):
+    context.move_to(point.x - .25, point.y)
+    context.line_to(point.x + .25, point.y)
+    context.move_to(point.x, point.y - .25)
+    context.line_to(point.x, point.y + .25)
+    context.stroke()
+
+def drawGrid(context):
+    context.set_line_width(.025)
+    context.set_source_rgb(.25, .25, .25)
+    for x in range(0, 101):
+        context.move_to(x, 0)
+        context.line_to(x, 50)
+    for y in range(51):
+        context.move_to(0, y)
+        context.line_to(100, y)
+    context.stroke()
+
 def drawFrame(ptArr):
-    with cairo.SVGSurface("frame.svg", 200, 200) as surface:
+    with cairo.SVGSurface("frame.svg", 100, 50) as surface:
         surface.set_document_unit(cairo.SVG_UNIT_CM)
         centerList = []
         context = cairo.Context(surface)
         ptArr.extend([[-1 * x[0], x[1]] for x in ptArr[-2::-1]])
         ptArr = [euclid.Point2(x[0], x[1]) for x in ptArr]
-        context.translate(100, 100)
-        context.scale(3, 3)
+        drawGrid(context)
+        context.translate(50, 50)
         context.rotate(math.pi)
         context.move_to(ptArr[0][0], ptArr[0][1])
         for i in range(1, len(ptArr)):
@@ -32,3 +50,4 @@ def drawFrame(ptArr):
             context.arc(center.x, center.y, .9525, 0, 2 * math.pi)
             context.set_source_rgb(0, 1, 0)
             context.stroke()
+            drawCenterMark(context, center)
